4cf29af4b41dcffb0ee4eecab9e884b58d068065
Joseph Gillette <os.joe.biz@pm.me>
2019-09-14 19:58:41 -0700

    Better Math

--- scripts/service-calculator.js
+++ scripts/service-calculator.js
@@ -1,1 +1,61 @@
-function calculate(){var e,o,n=document.getElementById("ipt1").value,t=document.getElementById("ipt2").value;e=findCharge(n,t),o=myTurnout(n,t),alert("You will charge "+e+" gp.\n\rYour service fee is "+o+" gp.")}function help(){console.log("Commands:"),console.log("findCharge(gePrice, itemAmount)"),console.log("myTurnout(gePrice, itemAmount)")}function findCharge(e,o){let n=e,t=void 0,l=void 0;return n-=t=.1*n,n*=o,n+=l=.25*n,n=Math.round(n)}function myTurnout(e,o){let n=e,t=void 0,l=void 0;return n-=t=.1*n,n=l=.25*(n*=o),n=Math.round(n)}console.log("Type help() for available options.");
+console.log("Type help() for available options.");
+
+function calculate() {
+  var itemAmount = document.getElementById("ipt1").value;
+  var itemCost = document.getElementById("ipt2").value;
+  
+  var perItem = perItemCharge(itemCost);
+  var totalCost = findCharge(itemAmount, itemCost);
+  var serviceFee = myTurnout(itemAmount, itemCost);
+
+  function b() {
+    return ("gp\n\r");
+  }
+
+  alert(
+    "Amount of Items : " + itemAmount + "\n\r" +
+    "Price per raw item: " + itemCost + b() +
+    "Price per finished item: " + perItem + b() +
+    "How much to charge: " + totalCost + b() +
+    "Labor cost: " + serviceFee + "gp"
+    );
+}
+
+function perItemCharge(price) {
+  let fprice = price;
+  let discount = undefined;
+  let fee = undefined;
+  discount = fprice * .10;
+  fprice -= discount;
+  fprice *= 1;
+  fee = fprice * .25;
+  fprice += fee;
+  fprice = Math.trunc(fprice);
+  return (fprice);
+}
+
+function findCharge(price, amount) {
+  let fprice = price;
+  let discount = undefined;
+  let fee = undefined;
+  discount = fprice * .10;
+  fprice -= discount;
+  fprice *= amount;
+  fee = fprice * .25;
+  fprice += fee;
+  fprice = Math.trunc(fprice);
+  return (fprice);
+}
+
+function myTurnout(price, amount) {
+  let fprice = price;
+  let discount = undefined;
+  let fee = undefined;
+  discount = fprice * .10;
+  fprice -= discount;
+  fprice *= amount;
+  fee = fprice * .25;
+  fprice = fee;
+  fprice = Math.trunc(fprice);
+  return (fprice);
+}
